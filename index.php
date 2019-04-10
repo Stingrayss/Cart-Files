b33b1dd6ab881d7ef8980f7bde5285239a01e76f
DUNCAN CADE <150041674@cart.ads>
2019-04-10 08:13:25 -0700

    First Commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Cade's First PHP Page</title>
+</head>
+<body>
+    <?php
+    echo "Hello World!";
+    ?>
+</body>
+</html>
